24fee028eb076304acb8be5ea00f932cd14c37d8
Nadezhda-Yarovaya <seriozz@yandex.ru>
2022-01-19 14:32:18 +0300

    поправлен функционал добавления в избранное

--- scripts/Note.js
+++ scripts/Note.js
@@ -1,58 +1,77 @@
+
 class Note {
-    constructor(text) {
-        this._text = text; 
+    constructor({ textNew }) {
+        this._text = textNew;
         this._template = document.querySelector(".addnew-template").content;
-        this._newNote= this._template.querySelector(".card")
+        this._newNote = this._template.querySelector(".card")
+        this.listStarred = document.querySelector(".notes-list__starred");
+        this.starredText = document.querySelector(".notes-list-stared");
+        this._listCards = document.querySelector(".notes-list__main");
     }
 
     _deleteNote() {
         this._finalNote.remove();
-        }
+    }
 
     _editNote() {
         this._finalNote.querySelector('.card__text').classList.toggle('card__text_hide');
         this._finalNote.querySelector('.card__form').classList.toggle('card__form_show');
-        }
+    }
 
-    _saveEditedNote(evt) {    
-            evt.preventDefault();
-            this._cardText.textContent = this._inputText.value;
-            this._cardText.classList.toggle('card__text_hide');
-            this._finalNote.querySelector('.card__form').classList.toggle('card__form_show');
-        }
-    
+    _saveEditedNote(evt) {
+        evt.preventDefault();
+        this._cardText.textContent = this._inputText.value;
+        this._cardText.classList.toggle('card__text_hide');
+        this._finalNote.querySelector('.card__form').classList.toggle('card__form_show');
+    }
 
-    _setEventListeners() {         
-        this._finalNote.querySelector('.card__button-delete').addEventListener('click',() => {
+    _setEventListeners() {
+        this._finalNote.querySelector('.card__button-delete').addEventListener('click', () => {
             this._deleteNote();
         });
 
-        this._finalNote.querySelector('.card__button-edit').addEventListener('click',() => {
-            this._editNote();
-        }); 
-
-        this._cardText.addEventListener('dblclick',() => {
+        this._finalNote.querySelector('.card__button-edit').addEventListener('click', () => {
             this._editNote();
         });
 
-        this._notesForm.addEventListener('submit',(evt) => {
+        this._finalNote.querySelector('.card__button-star').addEventListener('click', () => {
+            this._starNote();
+        });
+
+
+        this._cardText.addEventListener('dblclick', () => {
+            this._editNote();
+        });
+
+        this._notesForm.addEventListener('submit', (evt) => {
             this._saveEditedNote(evt);
-        }); 
+        });
     }
 
-addNewNote() {
-    this._finalNote = this._newNote.cloneNode(true);
-    this._cardText = this._finalNote.querySelector('.card__text');
-    this._inputText = this._finalNote.querySelector('.card__input-text');
-    this._notesForm = this._finalNote.querySelector(".card__form"); /*to edit*/
-    this._textEditInForm = this._notesForm.querySelector(".card__input-text");
 
-    this._setEventListeners();
-    this._cardText.textContent = this._text;
-    this._textEditInForm.value = this._text;
-    return this._finalNote;
-  }
+    _starNote(evt) {
+        this._starElement = this._finalNote.querySelector('.card__button-star');
+        this._starElement.classList.toggle('card__button-star_starred');
+        this._appendToStarOrNot = this._starElement.classList.contains('card__button-star_starred');
+        this._appendToStarOrNot ? this.listStarred.append(this._finalNote) : this._listCards.append(this._finalNote);
+        this._allStarredCards = this.listStarred.querySelectorAll('.card');
+        (this._allStarredCards.length === 0) ? this.starredText.classList.remove('notes-list-stared_none') : this.starredText.classList.add('notes-list-stared_none');
+    }
+
+    addNewNote() {
+        this._finalNote = this._newNote.cloneNode(true);
+        this._cardText = this._finalNote.querySelector('.card__text');
+        this._inputText = this._finalNote.querySelector('.card__input-text');
+        this._notesForm = this._finalNote.querySelector(".card__form"); /*to edit*/
+        this._textEditInForm = this._notesForm.querySelector(".card__input-text");
+
+        this._setEventListeners();
+
+        this._cardText.textContent = this._text;
+        this._textEditInForm.value = this._text;
+        return this._finalNote;
+    }
 
 }
 
-export {Note};
+export { Note };
--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,17 @@
-import {Note} from './Note.js';
+import { Note } from './Note.js';
 const form = document.forms.addanote;
 const inputForm = form.elements.addtext;
-const submitForm = form.elements.submitform;
 const listCards = document.querySelector(".notes-list__main");
-const listStarred = document.querySelector(".notes-list__starred");
-const starredText = document.querySelector(".notes-list-stared");
-/* delete all not necessary constants */
+const initialNotes = ["Купить хлеб", "Погулять", "Поиграть с собакой"];
 
-/* на новодобавленном не хочет работать функционал 
-тут наверно про время добавления !! будет ли с классом работать? */
+initialNotes.forEach((text) => {
+    const thisCard = makeNewNote(text);
+    listCards.append(thisCard);
+});
 
-const initialText = ["Купить хлеб", "Погулять", "Поиграть с собакой"];
-
-/* initial cards */
-initialText.forEach((text) => {
-    const thisCard = makeNewNote(text);  
-    listCards.append(thisCard); /* в конце списка */   
-    });
-
-function makeNewNote(textNew){    
-    const newNote = new Note(textNew);
-   return newNote.addNewNote();
+function makeNewNote(text) {
+    const newNote = new Note({ textNew: text });
+    return newNote.addNewNote();
 }
 
 form.addEventListener("submit", (evt) => {
@@ -28,36 +19,4 @@ form.addEventListener("submit", (evt) => {
     const thisCard = makeNewNote(inputForm.value);
     listCards.append(thisCard);
     form.reset();
-});
-
-/* all stars also transfer AND add duplicate */
-function starNote(evt) {
-    const currentCard = evt.currentTarget.closest('.card');
-    currentCard.querySelector('.card__button-star').classList.toggle('card__button-star_starred');
-    const appendToStarOrNot = currentCard.querySelector('.card__button-star').classList.contains('card__button-star_starred');
-    if (appendToStarOrNot) {
-    listStarred.append(currentCard); /*auto delete from listCards*/
-    } else {
-        listCards.append(currentCard); 
-    }
-    /* check if list of stars empty*/
-    const allStarredCards = listStarred.querySelectorAll('.card');
-    if (allStarredCards.length === 0) {
-        starredText.classList.remove('notes-list-stared_none');
-    }
-    else {
-        starredText.classList.add('notes-list-stared_none');
-    }
-}
-
-
-
-
-const allCards = document.querySelectorAll('.card');
-allCards.forEach((card) => {
-
-card.querySelector('.card__button-star').addEventListener('click',(evt) => {
-    starNote(evt);
-}); 
-
 });
